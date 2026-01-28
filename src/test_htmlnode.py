b8ec49c928282c5d3564b14fb8abe2ae21cb72c2
listlessneko <98135875+listlessneko@users.noreply.github.com>
2026-01-28 08:15:50 -0800

    Created leafnode and tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -57,6 +57,35 @@ class TestHTMLNode(unittest.TestCase):
             props=None
         )
         self.assertEqual(node.props_to_html(), '')
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_value_none(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_leaf_to_html_props_single(self):
+        node = LeafNode("a", "Foo Bar", {"href": "https://example.com"})
+        self.assertEqual(node.to_html(), '<a href="https://example.com">Foo Bar</a>')
+
+    def test_leaf_to_html_props_multiple(self):
+        node = LeafNode("a", "Foo Bar", {"href": "https://example.com", "target": "_blank"})
+        self.assertEqual(node.to_html(), '<a href="https://example.com" target="_blank">Foo Bar</a>')
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Just foo")
+        self.assertEqual(node.to_html(), "Just foo")
+
+    def test_leaf_to_html_empty_string(self):
+        node = LeafNode("p", "")
+        self.assertEqual(node.to_html(), "<p></p>")
+
+    def test_leaf_repr_(self):
+        node = LeafNode("p", "foo", {"class": "greeting"})
+        self.assertEqual(repr(node), "LeafNode(p, foo, {'class': 'greeting'})")
 
 if __name__ == "__main__":
     unittest.main()
